4d1bc2242941e1722fcd9fc0113640afe7ee0daa
Juan Manuel Ostiz <w4mp1z@outlook.com>
2022-09-01 15:51:08 -0300

    Correcciones mayores a la persistencia de las credenciales

--- src/config.py
+++ src/config.py
@@ -6,16 +6,27 @@ def get_config_path():
     return os.path.join(os.getcwd(), 'config.bat')
 
 
+def dict_to_binary(credentials):
+    binary = ' '.join(format(ord(letter), 'b') for letter in credentials)
+    return binary
+
+
+def binary_to_dict(credentials):
+    credentials = ''.join(chr(int(x, 2)) for x in credentials.split())
+    return json.loads(credentials)
+
+
 def persisted_config():
     if os.path.exists(get_config_path()):
         with open(get_config_path(), 'rb') as file:
-            data = file.read()
-            return json.loads(data)
+            credentials = file.read()
+            return binary_to_dict(credentials)
     else:
         return None
 
 
 def create_config(username, password):
     with open(get_config_path(), 'wb') as file:
-        data = json.dumps({'user': username, 'password': password})
-        file.write(data)
+        credentials = json.dumps({'user': username, 'password': password})
+        credentials = dict_to_binary(credentials)
+        file.write(bytes(credentials.encode('utf-8')))
